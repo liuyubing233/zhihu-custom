b6359d7cbf3f409ffde2db1cd0cfb840ca9d11c9
liuyubing <k549873769@163.com>
2021-03-24 14:38:40 +0800

    change version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎样式修改器
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.2.0
 // @description  知乎样式修改器-支持版心修改，左右模块位置调整、隐藏，夜间模式，logo隐藏，页面标头修改，自定义样式等
 // @author       super puffer fish
 // @match         *://www.zhihu.com/*
