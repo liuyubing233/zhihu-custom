1ab0377f5b2399333c1f1832aac8c3778ebed45f
liuyubing <k549873769@163.com>
2021-04-06 15:40:11 +0800

    bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎样式修改器
 // @namespace    http://tampermonkey.net/
-// @version      1.5.6
+// @version      1.5.7
 // @description  知乎样式自定义修改器-支持夜间模式、模块隐藏、可配置及自定义样式、图片全部可预览、过滤广告等
 // @author       pufferfish
 // @match         *://www.zhihu.com/*
@@ -18,7 +18,7 @@
   'use strict'
 
   // 开发使用 勿动 ---- start
-  const isDev = true
+  const isDev = false
   const INNER_HTML = null
   const INNER_CSS = null
   // 开发使用 勿动 ---- end
