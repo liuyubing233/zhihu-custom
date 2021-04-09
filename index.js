206d95602b05543474e343da36c4d8b30e656c3b
liuyubing <k549873769@163.com>
2021-04-09 17:07:05 +0800

    正则错误

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎样式修改器
 // @namespace    http://tampermonkey.net/
-// @version      1.7.1
+// @version      1.7.2
 // @description  一键极简模式，去除不必要的元素（可自动配置，随时还原），给你最简单的知乎；设置过滤烦人的故事档案局和盐选科普回答，并可一键过滤所有知乎官方账号回答；首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置；支持版心修改，页面模块位置调整、隐藏，页面表头和图标修改；页面背景色修改，黑色为夜间模式；列表的问题，文章和视频添加区分标签；去除广告，外链直接打开；更多功能请在插件里体验；想要的最终功能是页面大部分模块全可配置，目前努力更新中...
 // @author       pufferfish
 // @match         *://www.zhihu.com/*
@@ -1071,7 +1071,7 @@
         if (pfConfig.removeZhihuOfficial) {
           // 知乎官方账号优先级最高
           const label = $(that).find('.AuthorInfo-name .css-n99yhz').attr('aria-label')
-          if (/知乎[\s]官方帐号/.test(label)) {
+          if (/知乎[\s]*官方帐号/.test(label)) {
             $(that).remove()
             lessNum++
           }
