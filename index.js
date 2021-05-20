90b2840b11842eaa3e0ed08e9a6c536bdd5d6606
liuyubing <k549873769@163.com>
2021-05-20 13:08:54 +0800

    样式修复

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器✈持续更新✈努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      2.1.23
+// @version      2.1.24
 // @description  一键极简模式，去除不必要的元素，给你最简单的知乎（可自动配置，随时还原）|列表种类和关键词强过滤内容（目前只针对标题进行过滤），关键词过滤后自动调用“不感兴趣”的接口，防止在其他设备上出现同样内容|可设置自动收起所有长回答或自动展开所有回答|未登录状态下问答和专栏移除登录弹窗|设置过滤烦人的故事档案局和盐选科普回答，并可一键过滤所有知乎官方账号回答|首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置|支持版心修改，页面模块位置调整、隐藏，页面表头和图标修改|页面背景色修改可调整|夜间模式开关|隐藏知乎热搜模块，体验纯净搜索|列表的问题，文章和视频添加区分标签|去除广告，可设置购买链接只显示文字还是隐藏，外链直接打开|更多功能请在插件里体验...
 // @author       super pufferfish
 // @match         *://www.zhihu.com/*
@@ -824,7 +824,7 @@
     const heartVC =
       `.QuestionHeader-main,.Profile-mainColumn,.CollectionsDetailPage-mainColumn`
       + `,.Collections-mainColumn,.Balance-Main,.Post-RichTextContainer,.ContentItem-time`
-      + `,.Post-content .RichContent-actions,.RichContent-actions.is-fixed,.Post-NormalMain .Post-Header`
+      + `,.Post-content .RichContent-actions,.css-8txec3,.Post-NormalMain .Post-Header`
       + `,.Post-NormalMain>div,.Post-NormalSub>div,.Post-topicsAndReviewer`
       + `,.css-1xy3kyp,.css-1voxft1,.WriteIndexLayout-main,.css-ny4o71`
       + `{width:${vHeart().vContent}!important;}`
