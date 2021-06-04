fe180e12f74e76ae4276cbe492b2c27437195d08
liuyubing <k549873769@163.com>
2021-06-04 16:30:55 +0800

    fixbug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器✈持续更新✈努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      2.5.3
+// @version      2.5.4
 // @description  页面模块可配置化|列表种类和关键词强过滤内容，关键词过滤后自动调用“不感兴趣”的接口，防止在其他设备上出现同样内容|视频一键下载|回答内容按照点赞数和评论数排序|设置自动收起所有长回答或自动展开所有回答|移除登录弹窗|设置过滤故事档案局和盐选科普回答等知乎官方账号回答|首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置|夜间模式开关及背景色修改|收藏夹导出为PDF|隐藏知乎热搜，体验纯净搜索|列表添加标签种类|去除广告|设置购买链接显示方式|外链直接打开|更多功能请在插件里体验...
 // @author       super pufferfish
 // @match        *://www.zhihu.com/*
@@ -1602,7 +1602,7 @@
 
   // 推荐列表最外层绑定事件
   function topStoryRecommendEvent() {
-    $('.Topstory-recommend')[0].onclick = function (event) {
+    $('.Topstory-recommend')[0] && ($('.Topstory-recommend')[0].onclick = function (event) {
       if (pfConfig.listOutPutNotInterested && event.target.className === 'ContentItem-title') {
         // 使用pointer-events: none 和伪元素、子元素使用pointer-events:auto来获取点击
         let dataZop = {}
@@ -1614,7 +1614,7 @@
         $(event.target).parents('.TopstoryItem').remove()
         GM_log(`[customize]移除一条内容，已调用过滤接口`)
       }
-    }
+    })
   }
 
   // 关键词过滤列表内容
