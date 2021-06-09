27e75f1701e7d1f537b9b539047a631f6cc6feb4
liuyubing <k549873769@163.com>
2021-06-09 16:57:45 +0800

    关键词

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器✈持续更新✈努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      2.5.16
+// @version      2.5.17
 // @description  页面模块可配置化|列表种类和关键词强过滤内容，关键词过滤后自动调用“不感兴趣”的接口，防止在其他设备上出现同样内容|视频一键下载|回答内容按照点赞数和评论数排序|设置自动收起所有长回答或自动展开所有回答|移除登录弹窗|设置过滤故事档案局和盐选科普回答等知乎官方账号回答|首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置|夜间模式开关及背景色修改|收藏夹导出为PDF|隐藏知乎热搜，体验纯净搜索|列表添加标签种类|去除广告|设置购买链接显示方式|外链直接打开|更多功能请在插件里体验...
 // @author       super pufferfish
 // @match        *://www.zhihu.com/*
@@ -1653,7 +1653,7 @@
       const routeURL = $(events[i]).find('[itemprop="url"]') && $(events[i]).find('[itemprop="url"]').attr('content')
       let isFindTitle = false
       words.forEach((w) => {
-        const rep = new RegExp(w)
+        const rep = new RegExp(w.toLowerCase())
         if (rep.test(title.toLowerCase())) {
           isFindTitle = true
           filterKeywordText += `【${w}】`
