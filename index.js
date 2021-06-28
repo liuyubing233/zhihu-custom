b18957967b0130f20fa6eacf86054433be03f3e8
superPufferfish <k549873769@163.com>
2021-06-28 17:09:58 +0800

    解决部分情况下JQuery未加载完成就调用的情况（start时）导致脚本未成功加载的问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器✈持续更新✈努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      2.5.27
+// @version      2.5.28
 // @description  页面模块可配置化|列表种类和关键词强过滤内容，关键词过滤后自动调用“不感兴趣”的接口，防止在其他设备上出现同样内容|视频一键下载|回答内容按照点赞数和评论数排序|设置自动收起所有长回答或自动展开所有回答|移除登录弹窗|设置过滤故事档案局和盐选科普回答等知乎官方账号回答|首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置|夜间模式开关及背景色修改|收藏夹导出为PDF|隐藏知乎热搜，体验纯净搜索|列表添加标签种类|去除广告|设置购买链接显示方式|外链直接打开|更多功能请在插件里体验...
 // @author       super pufferfish
 // @match        *://*.zhihu.com/*
@@ -2024,7 +2024,8 @@
   }
 
   // 在启动时注入的内容
-  ; (async function () {
+  async function startInit () {
+    if (!$) return
     $('head').append(`<style type="text/css" id="pf-css-own">${INNER_CSS}</style>`)
     if (HTML_HOOTS.includes(location.hostname)) {
       timeStart = performance.now()
@@ -2068,7 +2069,8 @@
         answerSortBy = location.search.match(/(?<=sort=)\w+/)[0]
       }
     }
-  })()
+  }
+  startInit()
 
   function findTheme() {
     // 开始进入先修改一次
@@ -2114,6 +2116,8 @@
 
   // 在页面加载完成时注入的内容
   window.onload = () => {
+    !$('#pf-css-own')[0] && startInit()
+
     if (HTML_HOOTS.includes(location.hostname)) {
       initHTML()
       initData()
