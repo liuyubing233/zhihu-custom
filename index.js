4a2c7154933a3ca8c86cbb782a2995094a90c9a7
superPufferfish <k549873769@163.com>
2021-09-18 11:04:17 +0800

    fix: 修复首页左右侧栏定位错误

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器✈持续更新✈努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      2.6.6
+// @version      2.6.7
 // @description  页面模块可配置化|列表种类和关键词强过滤内容，关键词过滤后自动调用“不感兴趣”的接口，防止在其他设备上出现同样内容|视频一键下载|回答内容按照点赞数和评论数排序|设置自动收起所有长回答或自动展开所有回答|移除登录弹窗|设置过滤故事档案局和盐选科普回答等知乎官方账号回答|首页切换模块，发现切换模块、个人中心、搜素栏可悬浮并自定义位置|夜间模式开关及背景色修改|收藏夹导出为PDF|隐藏知乎热搜，体验纯净搜索|列表添加标签种类|去除广告|设置购买链接显示方式|外链直接打开|屏蔽用户回答|更多功能请在插件里体验...
 // @author       super pufferfish
 // @match        *://*.zhihu.com/*
@@ -603,17 +603,31 @@ const LEAST_HEART = '1000';
       window.scrollY > 0 ? stickyB.fixed() : stickyB.inherit()
     },
     fixed: function () {
+      const dScrollHeader = $('.AppHeader.is-fixed')[0]
+      const dH = dScrollHeader ? dScrollHeader.offsetHeight : 0
+
       // 左侧盒子
       if (pfConfig.stickyLeft && $(C_STICKY_LEFT_DAD)[0]) {
         const { offsetWidth, offsetLeft, offsetTop } = $(C_STICKY_LEFT_DAD)[0]
-        $(C_STICKY_LEFT).css({ position: 'fixed', width: offsetWidth, left: offsetLeft, top: offsetTop })
+        const offsetLeftParent = $(C_STICKY_LEFT_DAD).parent()[0].offsetLeft
+        $(C_STICKY_LEFT).css({
+          position: 'fixed',
+          width: offsetWidth,
+          left: offsetLeft + offsetLeftParent,
+          top: offsetTop + dH + 10
+        })
       } else {
         $(C_STICKY_LEFT).removeAttr('style', '')
       }
       // 右侧盒子
       if (pfConfig.stickyRight && $(C_STICKY_RIGHT_DAD)[0]) {
         const { offsetWidth, offsetRight, offsetTop } = $(C_STICKY_RIGHT_DAD)[0]
-        $(C_STICKY_RIGHT).css({ position: 'fixed', width: offsetWidth, right: offsetRight, top: offsetTop })
+        $(C_STICKY_RIGHT).css({
+          position: 'fixed',
+          width: offsetWidth,
+          right: offsetRight,
+          top: offsetTop + dH + 10
+        })
       } else {
         $(C_STICKY_RIGHT).removeAttr('style', '')
         $(C_STICKY_RIGHT)[0] && ($(C_STICKY_RIGHT)[0].style = 'position: inherit!important')
