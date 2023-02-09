a1804e76acbb8d025ef718d9fd554a8f393c6b1f
liuyubing <k549873769@163.com>
2023-02-09 16:12:38 +0800

    fix: fix tampermonkey cannot update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器🤜持续更新🤛努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      3.7.1
+// @version      3.7.2
 // @description  页面模块自定义隐藏|列表及回答内容过滤|保存浏览历史记录|推荐页内容缓存|列表种类和关键词强过滤，自动调用「不感兴趣」接口|屏蔽用户回答|回答视频下载|回答内容按照点赞数和评论数排序|设置自动收起所有长回答或自动展开所有回答|移除登录提示弹窗|设置过滤故事档案局和盐选科普回答等知乎官方账号回答|手动调节文字大小|切换主题，夜间模式调整|隐藏知乎热搜，体验纯净搜索|列表添加标签种类|去除广告|设置购买链接显示方式|收藏夹内容导出为 PDF|一键移除所有屏蔽选项|外链直接打开|更多功能请在插件里体验...
 // @compatible   edge Violentmonkey
 // @compatible   edge Tampermonkey
@@ -607,12 +607,8 @@ const EXTRA_CLASS_HTML = {
     cachePfConfig: {}, // 缓存初始配置
     cacheTitle: '', // 缓存页面原标题
     fetchHeaders: {}, // fetch 的 headers 内容, 获取下来以供使用
-    // xZst81: '',
     heightForList: 0, // 列表缓存高度
   };
-
-  /** 请求头内容缓存 */
-  const catchRequestHeader = {};
 
   /** 修改页面背景的 css */
   const myBackground = {
