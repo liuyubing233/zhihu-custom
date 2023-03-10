c1f2499982bba33c2dc0e576195beeb81ea5e2ec
liuyubing <k549873769@163.com>
2023-03-10 16:57:08 +0800

    fix: 3.9.1 解决按下空格隐藏的视频会自动播放的问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器🤜持续更新🤛努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      3.9.0
+// @version      3.9.1
 // @description  页面模块自定义隐藏，列表及回答内容过滤，保存浏览历史记录，推荐页内容缓存，列表种类和关键词强过滤并自动调用「不感兴趣」接口，屏蔽用户回答，回答视频下载，回答内容按照点赞数和评论数排序，设置自动收起所有长回答或自动展开所有回答，移除登录提示弹窗，设置过滤故事档案局和盐选科普回答等知乎官方账号回答，手动调节文字大小，切换主题及夜间模式调整，隐藏知乎热搜，列表添加标签种类，去除广告，设置购买链接显示方式，收藏夹内容导出为PDF，一键移除所有屏蔽选项，外链直接打开，更多功能请在插件里体验...
 // @compatible   edge Violentmonkey
 // @compatible   edge Tampermonkey
@@ -1645,7 +1645,7 @@
       const classBox = 'ctz-block-box';
       event.querySelector(`.${classBox}`) && event.querySelector(`.${classBox}`).remove();
       const nodeUser = event.querySelector('.AnswerItem-authorInfo>.AuthorInfo');
-      if (!nodeUser.offsetHeight) return;
+      if (!nodeUser || !nodeUser.offsetHeight) return;
       const userUrl = nodeUser.querySelector('meta[itemprop="url"]').content;
       const userName = nodeUser.querySelector('meta[itemprop="name"]').content;
       const avatar = nodeUser.querySelector('meta[itemprop="image"]').content;
@@ -2724,6 +2724,20 @@
     }
   }
 
+  /** 解决视频自动播放问题 */
+  const fixVideoAutoPlay = () => {
+    // 拦截 video.play() 指令
+    var originalPlay = HTMLMediaElement.prototype.play;
+    HTMLMediaElement.prototype.play = function () {
+      // 如果视频隐藏则退出
+      if (!this.offsetHeight) {
+        return;
+      }
+      // 否则正常执行 video.play() 指令
+      return originalPlay.apply(this, arguments);
+    };
+  };
+
   /** 添加浏览历史 */
   const initHistoryView = async () => {
     const question = 'www.zhihu.com/question/';
@@ -2754,7 +2768,7 @@
       isHaveHeadWhenInit = false;
       return;
     }
-
+    fixVideoAutoPlay();
     fnInitDomStyle('CTZ_STYLE', INNER_CSS);
     if (!HTML_HOOTS.includes(location.hostname) || window.frameElement) return;
     storageConfig.cachePfConfig = pfConfig;
