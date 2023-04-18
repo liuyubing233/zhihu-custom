39e201493435794ff1ee2afc18603d23a3013626
liuyubing <k549873769@163.com>
2023-04-18 11:16:09 +0800

    feat:  物理隐藏顶部推广（会存在颜色错误问题）

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器🤜持续更新🤛努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      3.14.3
+// @version      3.14.4
 // @description  页面模块自定义隐藏，列表及回答内容过滤，保存浏览历史记录，推荐页内容缓存，列表种类和关键词强过滤并自动调用「不感兴趣」接口，屏蔽用户回答，回答视频下载，回答内容按照点赞数和评论数排序，设置自动收起所有长回答或自动展开所有回答，移除登录提示弹窗，设置过滤故事档案局和盐选科普回答等知乎官方账号回答，手动调节文字大小，切换主题及夜间模式调整，隐藏知乎热搜，列表添加标签种类，去除广告，设置购买链接显示方式，收藏夹内容导出为PDF，一键移除所有屏蔽选项，外链直接打开，更多功能请在插件里体验...
 // @compatible   edge Violentmonkey
 // @compatible   edge Tampermonkey
@@ -449,6 +449,7 @@
     /** 基础设置 */
     CTZ_SET_BASIS: [
       [{ value: 'hiddenAD', label: '广告' }],
+      [{ value: 'hiddenTopAD', label: '顶部推广（只能物理隐藏，会存在错误问题）' }],
       [
         { value: 'hiddenLogo', label: 'logo' },
         { value: 'hiddenHeader', label: '顶部悬浮模块' },
@@ -1121,6 +1122,7 @@
       hiddenQuestionComment: '.QuestionPage .QuestionHeader .QuestionHeader-Comment{display: none}',
       hiddenQuestionMore: '.QuestionPage .QuestionHeader [aria-label="更多"]{display: none;}',
       hiddenOpenButton: '#CTZ_OPEN_BUTTON{display: none;}',
+      hiddenTopAD: '.App-main .Topstory>div:first-of-type:not(.Topstory-container){display: none}',
     },
     cssForKeysArray: [
       {
