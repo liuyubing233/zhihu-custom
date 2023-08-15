62dcd56a293507439a2678965c7d74bfeeabe55b
liuyubing <k549873769@163.com>
2023-08-15 10:18:46 +0800

    fix: 修复隐藏专栏顶部图片导致的首页列表图片不显示的问题

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎修改器🤜持续更新🤛努力实现功能最全的知乎配置插件
 // @namespace    http://tampermonkey.net/
-// @version      3.16.2
+// @version      3.16.3
 // @description  页面模块自定义隐藏，列表及回答内容过滤，保存浏览历史记录，推荐页内容缓存，一键邀请，复制代码块删除版权信息，列表种类和关键词强过滤并自动调用「不感兴趣」接口，屏蔽用户回答，回答视频下载，回答内容按照点赞数和评论数排序，设置自动收起所有长回答或自动展开所有回答，移除登录提示弹窗，设置过滤故事档案局和盐选科普回答等知乎官方账号回答，手动调节文字大小，切换主题及夜间模式调整，隐藏知乎热搜，列表添加标签种类，去除广告，设置购买链接显示方式，收藏夹内容导出为PDF，一键移除所有屏蔽选项，外链直接打开，更多功能请在插件里体验...
 // @compatible   edge Violentmonkey
 // @compatible   edge Tampermonkey
@@ -916,7 +916,7 @@
         `{display: none;}`,
       hiddenListVideoContent: `.Topstory-recommend .ZVideoItem-video,.Topstory-recommend .VideoAnswerPlayer,.Topstory-recommend .ZVideoItem .RichContent{display: none;}`,
       hiddenZhuanlanActions: '.RichContent-actions.is-fixed>.ContentItem-actions{display: none;}',
-      hiddenZhuanlanTitleImage: '.TitleImage,.css-78p1r9,.RichContent>div:first-of-type>div:last-of-type{display: none;!important}',
+      hiddenZhuanlanTitleImage: '.ArticleItem .TitleImage,.ArticleItem .css-78p1r9,.ArticleItem .RichContent>div:first-of-type:not(.RichContent-cover)>div:last-of-type{display: none!important;}',
       hiddenFixedActions: `.ContentItem .RichContent-actions.is-fixed,.List-item .RichContent-actions.is-fixed{visibility: hidden!important;}`,
       hiddenHotItemMetrics: '.HotItem-content .HotItem-metrics{display: none;}',
       hiddenHotItemIndex: '.HotItem-index{display: none;}.HotItem{padding: 16px!important;}',
