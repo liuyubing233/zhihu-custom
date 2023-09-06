88d4a291f72e5ccbb60c1fc36aa93e259bfeedd6
liuyubing <k549873769@163.com>
2023-09-06 17:43:52 +0800

    feat: remove src backup

--- src-backup/variable/filters.js
+++
@@ -1,6 +0,0 @@
-/** 屏蔽关注列表关注人操作 */
-export const FILTER_FOLLOWER_OPERATE = [
-  { key: 'removeFollowVoteAnswer', rep: '赞同了回答' },
-  { key: 'removeFollowVoteArticle', rep: '赞同了文章' },
-  { key: 'removeFollowFQuestion', rep: '关注了问题' },
-];
--- src-backup/variable/history.js
+++
@@ -1,7 +0,0 @@
-/** 缓存历史记录 */
-let pfHistory = {
-  list: [],
-  view: [],
-};
-/** 缓存的历史记录数量 */
-const SAVE_HISTORY_NUMBER = 500;
