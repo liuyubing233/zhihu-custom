21251aa5f0fdc96af54c967e761550cc7687e13b
liuyubing <k549873769@163.com>
2023-10-09 13:40:30 +0800

    feat: auto tag change

--- auto-tag.js
+++ auto-tag.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
+const { exec, echo } = shell;
 
-const { exec } = shell;
-
-const status = process.argv[process.argv.length - 1]
+const status = process.argv[process.argv.length - 1];
 const prevVersion = process.env.npm_package_version;
 const [vMajor, vMinor, vRevision] = prevVersion.split('.');
 const changeVersion = {
-  'fix': `${vMajor}.${vMinor}.${+vRevision + 1}`,
-  'feature': `${vMajor}.${+vMinor + 1}.0`,
-  'release': `${+vMajor + 1}.0.0`,
-}
+  fix: `${vMajor}.${vMinor}.${+vRevision + 1}`,
+  feature: `${vMajor}.${+vMinor + 1}.0`,
+  release: `${+vMajor + 1}.0.0`,
+};
 
 if (!changeVersion[status]) {
-  console.log(`自动打标参数必须为以下其中一种:
+  echo(`自动打标参数必须为以下其中一种:
   fix      问题修改&功能优化(0.0.0 ---> 0.0.1),
   feature  功能添加(0.0.0 ---> 0.1.0),
   release  大版本更新(0.0.0 ---> 1.0.0)`);
@@ -22,13 +21,26 @@ if (!changeVersion[status]) {
 }
 
 const nVersion = changeVersion[status];
-// console.log(prevVersion, nVersion);
-// console.log(status, process.env.npm_package_version);
-
-const regExpVersion = new RegExp(`("version":\\s*")([\\d\\.]+)(")`)
+const regExpVersion = new RegExp(`("version":\\s*")([\\d\\.]+)(")`);
 const pathPackageJson = path.join(__dirname, '/package.json');
-const packageJson = fs.readFileSync(pathPackageJson).toString()
-fs.writeFileSync(pathPackageJson, packageJson.replace(regExpVersion, `$1${nVersion}$3`))
+const packageJson = fs.readFileSync(pathPackageJson).toString();
+fs.writeFileSync(pathPackageJson, packageJson.replace(regExpVersion, `$1${nVersion}$3`));
+echo(`package.json 文件版本号修改完成。\r\n原版本号: ${prevVersion}，新版本号: ${nVersion}`);
 
-// console.log(packageJson.replace(regExpVersion, `$1${nVersion}$3`));
+const doExec = async (commit) => {
+  const res = exec(commit);
+  if (res.code !== 0) {
+    echo(`ERROR: ${commit}: ${code.stderr}`);
+    return Promise.reject();
+  }
+  return Promise.resolve();
+};
 
+(async function () {
+  await doExec('yarn build');
+  await doExec('git add .');
+  await doExec(`git commit -m "docs: v${nVersion}"`);
+  await doExec(`git push`);
+  await doExec(`git tag v${nVersion}--text`)
+  await doExec(`git push --tag`)
+})();
