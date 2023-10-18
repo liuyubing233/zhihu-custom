a78749eda444fd42c874d7ff333603770a6df562
liuyubing <k549873769@163.com>
2023-10-18 11:22:15 +0800

    chore: use plugin to compress less

--- esbuild.plugins.js
+++ esbuild.plugins.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const less = require('less');
 
 /** 加载 HTML 和 CSS 资源 */
 const envInnerResources = {
   name: 'envInnerResources',
   setup(build) {
-    build.onLoad({ filter: /web-resources.ts/ }, (args) => {
+    build.onLoad({ filter: /web-resources.ts/ }, async (args) => {
+      const contentLess = fs.readFileSync(path.join(__dirname, '/src/static/index.less'), 'utf-8');
+      const res = await less.render(contentLess, { compress: true });
+      if (!res.css) {
+        throw Error('less转css失败');
+      }
       const NAME_HTML = 'INNER_HTML';
       const NAME_CSS = 'INNER_CSS';
       const pathHTML = path.join(__dirname, '/src/static/index.html');
-      const pathCSS = path.join(__dirname, '/src/static/index.css');
       const REGEXP_REPLACE = /\s*\n\s*/g; // 删除回车及前后空格
       const REGEXP_REPLACE_COMMIT = /\<\!\-\-[^(\<\!)]*\-\-\>/g; // 删除HTML注释
       const strHTML = fs.readFileSync(pathHTML).toString();
-      const strCSS = fs.readFileSync(pathCSS).toString();
       const prevContent = fs.readFileSync(args.path).toString();
       const nRegExp = (name) => new RegExp('(' + name + '\\s\\=\\s`)()(`)');
       const regexpHTML = nRegExp(NAME_HTML);
       const innerHTML = strHTML.replace(REGEXP_REPLACE, '').replace(REGEXP_REPLACE_COMMIT, '');
       const regexpCSS = nRegExp(NAME_CSS);
-      const innerCSS = strCSS.replace(REGEXP_REPLACE, '');
+      const innerCSS = res.css.replace(REGEXP_REPLACE, '');
       return { contents: prevContent.replace(regexpHTML, `$1${innerHTML}$3`).replace(regexpCSS, `$1${innerCSS}$3`) };
     });
   },
