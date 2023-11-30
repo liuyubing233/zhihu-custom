a5916d9a44ea9b6d0f2b2fa786e58341919a107d
liuyubing <k549873769@163.com>
2023-11-30 14:38:10 +0800

    feat: 更新视频标签，解决知乎类名修改视频标签不显示的问题

--- src/methods/version.ts
+++ src/methods/version.ts
@@ -97,7 +97,7 @@ export const myVersion = {
     return fnReturnStr(
       `.AnswerItem .ContentItem-title::before{content:'问答';background:#ec7259}` +
         `.TopstoryItem .PinItem::before{content:'想法';background:#9c27b0;${cssTag}}.PinItem>.ContentItem-title{margin-top:4px;}` +
-        `.ZvideoItem .ContentItem-title::before{content:'视频';background:#12c2e9}` +
+        `.ZvideoItem .ContentItem-title::before{content:'视频';background:#12c2e9}.ZVideoItem .ContentItem-title::before{content:'视频';background:#12c2e9}` +
         `.ArticleItem .ContentItem-title::before{content:'文章';background:#00965e}` +
         `.ContentItem .ContentItem-title::before{margin-right:6px;font-weight:normal;display:inline;padding:2px 4px;border-radius:4px;font-size:12px;color:#ffffff}` +
         `.TopstoryQuestionAskItem .ContentItem-title::before{content:'提问';background:#533b77}`,
