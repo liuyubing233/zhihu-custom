9cab4da05111a96dce33b9df3b8530d5ebd8d677
liuyubing <k549873769@163.com>
2024-01-11 15:15:37 +0800

    fix: 修复部分情况下一键邀请按钮消失的问题。

--- src/init/init-invite-once.ts
+++ src/init/init-invite-once.ts
@@ -2,35 +2,38 @@ import { dom, domA, domC } from '../commons/tools';
 
 /** 添加一键邀请功能 */
 export const initInviteOnce = () => {
-  const domInvitation = dom('.QuestionInvitation');
-  if (!domInvitation) return;
-  const nButton = domC('button', {
-    className: 'ctz-button',
-    innerHTML: '一键邀请',
-  });
-  nButton.onclick = () => {
-    const fnToMore = () => {
-      const moreAction = dom('.QuestionMainAction');
-      if (moreAction) {
-        moreAction.click();
-        setTimeout(() => {
-          fnToMore();
-        }, 50);
-      } else {
-        fnToInviteAll();
-      }
+  setTimeout(() => {
+    const domInvitation = dom('.QuestionInvitation');
+    if (!domInvitation || dom('.ctz-invite-once')) return;
+    const nButton = domC('button', {
+      className: 'ctz-button ctz-invite-once',
+      innerHTML: '一键邀请',
+      style: 'margin-left: 12px;'
+    });
+    nButton.onclick = () => {
+      const fnToMore = () => {
+        const moreAction = dom('.QuestionMainAction');
+        if (moreAction) {
+          moreAction.click();
+          setTimeout(() => {
+            fnToMore();
+          }, 50);
+        } else {
+          fnToInviteAll();
+        }
+      };
+
+      const fnToInviteAll = () => {
+        const nodeInvites = domA('.QuestionInvitation .ContentItem-extra button');
+        nodeInvites.forEach((item) => {
+          !item.disabled && !item.classList.contains('AutoInviteItem-button--closed') && item.click();
+        });
+      };
+
+      fnToMore();
     };
 
-    const fnToInviteAll = () => {
-      const nodeInvites = domA('.QuestionInvitation .ContentItem-extra button');
-      nodeInvites.forEach((item) => {
-        !item.disabled && !item.classList.contains('AutoInviteItem-button--closed') && item.click();
-      });
-    };
-
-    fnToMore();
-  };
-
-  const nodeTopBar = domInvitation.querySelector('.Topbar');
-  nodeTopBar && nodeTopBar.appendChild(nButton);
+    const nodeTopBar = domInvitation.querySelector('.Topbar');
+    nodeTopBar && nodeTopBar.appendChild(nButton);
+  }, 500);
 };
