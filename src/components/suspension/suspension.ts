b0101260d3373c7865ce4b944628ce3350b63d1f
liuyubing <k549873769@163.com>
2025-03-05 11:16:52 +0800

    fix: 修复悬浮搜索栏模块时可能会出现两个搜索标志的问题；

--- src/components/suspension/suspension.ts
+++ src/components/suspension/suspension.ts
@@ -1,12 +1,11 @@
-import { myStorage } from '../commons/storage';
-import { dom, domC } from '../commons/tools';
-import { store } from '../store';
-import { IHeaderDoms } from '../types';
-import { myLock } from './lock';
-import { myMove } from './move';
-import { myVersion } from './version';
+import { myStorage } from '../../commons/storage';
+import { dom, domC } from '../../commons/tools';
+import { store } from '../../store';
+import { IHeaderDoms } from '../../types';
+import { myMove } from '../move';
+import { myVersion } from '../version';
 
-/** 是否开启漂浮收起按钮的方法 */
+/** 修改唤起修改器弹窗按钮的悬浮方式 */
 export const suspensionPickupAttribute = async () => {
   const { suspensionPickUp } = await myStorage.getConfig();
   if (suspensionPickUp) {
@@ -37,6 +36,10 @@ export const cacheHeader = async () => {
     eventHeader.fun && clearTimeout(eventHeader.fun);
     eventHeader.fun = setTimeout(() => {
       if (eventHeader.num < 100) {
+        const prevFind = dom('.AppHeader-inner .AppHeader-Tabs');
+        const prevSearch = dom('.AppHeader-inner .SearchBar');
+        const prevUser = dom('.AppHeader-inner .AppHeader-userInfo');
+
         if (dom('.AppHeader-inner')) {
           eventHeader.isFind = true;
           setStorageConfigItem('headerDoms', {
@@ -73,13 +76,16 @@ export const cacheHeader = async () => {
     if (pfConfig[name]) {
       // 如果是 suspensionSearch 则添加展开和收起按钮
       if (name === 'suspensionSearch') {
-        !dom(classIcon) && even.appendChild(domC('i', { className: 'ctz-search-icon', innerHTML: '⚲' }));
-        !dom(classPickup) && even.appendChild(domC('i', { className: 'ctz-search-pick-up', innerHTML: '⇤' }));
-        if (dom(classIcon)) {
-          dom(classIcon)!.onclick = () => even.classList.add(classNameFocus);
+        if (!even.querySelector(classIcon)) {
+          const nDomSearch = domC('i', { className: 'ctz-search-icon', innerHTML: '⚲' });
+          nDomSearch.onclick = () => even.classList.add(classNameFocus);
+          even.appendChild(nDomSearch);
         }
-        if (dom(classPickup)) {
-          dom(classPickup)!.onclick = () => even.classList.remove(classNameFocus);
+
+        if (!even.querySelector(classPickup)) {
+          const nDomPickup = domC('i', { className: 'ctz-search-pick-up', innerHTML: '⇤' });
+          nDomPickup.onclick = () => even.classList.remove(classNameFocus);
+          even.appendChild(nDomPickup);
         }
       }
       myLock.append(even, name);
@@ -120,4 +126,48 @@ const cSuspensionStyle = async (name: string) => {
   if (cssObj[name]) {
     pfConfig[name] ? myMove.init(cssObj[name], `${name}Po`, name) : myMove.destroy(cssObj[name]);
   }
+};
+
+/** 悬浮模块开关锁添加移除方法 */
+const myLock = {
+  append: async function (e: HTMLElement, name: string) {
+    // 悬浮模块是否固定改为鼠标放置到模块上显示开锁图标 点击即可移动模块
+    if (!e) return;
+    const lock = this.lock.class;
+    const unlock = this.unlock.class;
+    const lockMask = this.lockMask.class;
+    const classRemove = 'ctz-move-this';
+    const iLock = domC('i', { className: `${this.lock.name}`, innerHTML: '☑︎' });
+    const iUnlock = domC('i', { className: `${this.unlock.name}`, innerHTML: '☒' });
+    const dLockMask = domC('div', { className: this.lockMask.name });
+    !e.querySelector(lock) && e.appendChild(iLock);
+    !e.querySelector(unlock) && e.appendChild(iUnlock);
+    !e.querySelector(lockMask) && e.appendChild(dLockMask);
+
+    const pfConfig = await myStorage.getConfig();
+    (e.querySelector(lock) as HTMLButtonElement).onclick = async () => {
+      await myStorage.updateConfigItem(name + 'Fixed', true);
+      e.classList.remove(classRemove);
+    };
+    (e.querySelector(unlock) as HTMLButtonElement).onclick = async () => {
+      await myStorage.updateConfigItem(name + 'Fixed', false);
+      e.classList.add(classRemove);
+    };
+    // 如果进入页面的时候该项的 FIXED 为 false 则添加 class
+    if (pfConfig[name + 'Fixed'] === false) {
+      e.classList.add(classRemove);
+    }
+  },
+  remove: function (e: HTMLElement) {
+    if (!e) return;
+    const nodeLock = e.querySelector(this.lock.class);
+    const nodeUnlock = e.querySelector(this.unlock.class);
+    const nodeLockMask = e.querySelector(this.lockMask.class);
+    nodeLock && nodeLock.remove();
+    nodeUnlock && nodeUnlock.remove();
+    nodeLockMask && nodeLockMask.remove();
+  },
+  lock: { class: '.ctz-lock', name: 'ctz-lock' },
+  unlock: { class: '.ctz-unlock', name: 'ctz-unlock' },
+  lockMask: { class: '.ctz-lock-mask', name: 'ctz-lock-mask' },
 };
